fa17a0f0035514ea1c70d1d34b12980264ffca82
Léon <dalleleon@gmail.com>
2022-12-29 11:36:04 +0100

    test test_ascii

---
+++ Code/test_ascii.c
@@ -0,0 +1,13 @@
+
+
+
+#include <stdio.h>
+
+int main() {
+    for(int i = 0; i < 256; i++) {
+        printf("%d : %c\n", i, i);
+    }
+    return 0;
+}
+
+// what does this code do : This code prints the ASCII values of all the characters from 0 to 255.
